ccf3cb40c80b55f989908e6ae0cd7b9d8f597694
Pat Lawler <patlaw777@icloud.com>
2022-05-08 18:54:06 -0500

    added portfolio component information

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header'
+import Gallery from '../src/components/Gallery'
+import Bio from '../src/components/Bio'
 import './App.css';
 
 function App() {
+
+  const [navItems] = useState([
+    'About Me',
+    'Portfolio',
+    'Contact',
+    'Resume'
+   ]);
+   
+  const [currentNavItem, setNavItem] = useState(navItems[0]);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `PJL - ${currentNavItem}`;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header navItems={navItems} currentNavItem={currentNavItem} setNavItem={setNavItem} />
+      <br />
+      {/* <p>A professional developer/coder who posseses an invaluable combination of education and experience coupled with an extreme passion and appitude for web/software developement</p> */}
+      {currentNavItem === 'About Me' && (<Bio />)}
+      {currentNavItem === 'Portfolio' && (<Gallery />)}
     </div>
   );
 }
 
 export default App;
+
